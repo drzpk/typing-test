4f41efc5d980a4f2a0f159424855a534a72fb81c
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2021-10-25 11:12:45 +0200

    Fix pending request exception handling

--- frontend/web/src/utils/store-utils.ts
+++ frontend/web/src/utils/store-utils.ts
@@ -1,18 +1,16 @@
 import {ActionContext} from "vuex";
 
 export function withPendingRequest<T>(requestName: string, context: ActionContext<any, any>, callback: () => Promise<T>): Promise<T> {
-    try {
-        return new Promise(resolve => {
-            context.commit("addPendingRequest", requestName);
-            resolve(null);
-        }).then(() => {
-            return callback();
-        }).then((result: T) => {
-            context.commit("clearPendingRequest", requestName);
-            return result;
-        });
-    } catch (e) {
+    return new Promise(resolve => {
+        context.commit("addPendingRequest", requestName);
+        resolve(null);
+    }).then(() => {
+        return callback()
+    }).then((result: T) => {
+        context.commit("clearPendingRequest", requestName);
+        return result;
+    }).catch(e => {
         context.commit("clearPendingRequest", requestName);
         throw e;
-    }
+    });
 }
