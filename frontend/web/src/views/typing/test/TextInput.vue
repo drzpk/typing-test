2086c9a1017d20e6194080098e10f3bcd625f728
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2021-11-21 20:18:30 +0100

    Formatting

--- frontend/web/src/views/typing/test/TextInput.vue
+++ frontend/web/src/views/typing/test/TextInput.vue
@@ -14,73 +14,73 @@ import {TestStateModel} from "@/models/tests";
 import {WordChangeEvent} from "@/models/events";
 
 @Component({
-        computed: {
-            ...mapGetters(["testState", "activeTestStarted"])
-        }
-    })
-    export default class TextInput extends Vue {
-        testState!: TestStateModel | undefined;
-        activeTestStarted!: boolean;
-
-        get inputDisabled(): boolean {
-            return this.testState !== TestStateModel.STARTED && this.testState !== TestStateModel.CREATED
-        }
-
-        get placeholder(): string {
-            if (this.testState == TestStateModel.CREATED)
-                return "Start typing to start the test.";
-            else
-                return "";
-        }
-
-        @Watch("testState")
-        onTestStateChanged(): void {
-            if (this.testState == TestStateModel.CREATED)
-                (document.getElementById("text-input-input") as HTMLInputElement).value = "";
-        }
-
-        onInputChanged(event: InputEvent): void {
-            this.startTestIfNecessary();
-
-            const element = event.target as HTMLInputElement;
-            const value = element.value || "";
-
-            const trimmedInput = value.trim();
-            const endsWithSpace = value.length > 0 ? value.charAt(value.length - 1) === " " : false;
-            const containsWord = trimmedInput.length > 0;
-
-
-            if (endsWithSpace || !containsWord)
-                element.value = "";
-
-            const completeWord = endsWithSpace && containsWord;
-
-            if (completeWord)
-                this.$store.commit("addEnteredWord", trimmedInput);
-            if (event.type === "deleteContentBackward")
-                this.$store.commit("incrementBackspaceCount");
-
-            const wordChangeEvent = new WordChangeEvent(trimmedInput, completeWord);
-            this.$root.$emit(WordChangeEvent.NAME, wordChangeEvent);
-        }
-
-        private startTestIfNecessary(): void {
-            if (!this.activeTestStarted && this.testState == TestStateModel.CREATED)
-                this.$store.dispatch("startTest");
-        }
+    computed: {
+        ...mapGetters(["testState", "activeTestStarted"])
     }
+})
+export default class TextInput extends Vue {
+    testState!: TestStateModel | undefined;
+    activeTestStarted!: boolean;
+
+    get inputDisabled(): boolean {
+        return this.testState !== TestStateModel.STARTED && this.testState !== TestStateModel.CREATED
+    }
+
+    get placeholder(): string {
+        if (this.testState == TestStateModel.CREATED)
+            return "Start typing to start the test.";
+        else
+            return "";
+    }
+
+    @Watch("testState")
+    onTestStateChanged(): void {
+        if (this.testState == TestStateModel.CREATED)
+            (document.getElementById("text-input-input") as HTMLInputElement).value = "";
+    }
+
+    onInputChanged(event: InputEvent): void {
+        this.startTestIfNecessary();
+
+        const element = event.target as HTMLInputElement;
+        const value = element.value || "";
+
+        const trimmedInput = value.trim();
+        const endsWithSpace = value.length > 0 ? value.charAt(value.length - 1) === " " : false;
+        const containsWord = trimmedInput.length > 0;
+
+
+        if (endsWithSpace || !containsWord)
+            element.value = "";
+
+        const completeWord = endsWithSpace && containsWord;
+
+        if (completeWord)
+            this.$store.commit("addEnteredWord", trimmedInput);
+        if (event.type === "deleteContentBackward")
+            this.$store.commit("incrementBackspaceCount");
+
+        const wordChangeEvent = new WordChangeEvent(trimmedInput, completeWord);
+        this.$root.$emit(WordChangeEvent.NAME, wordChangeEvent);
+    }
+
+    private startTestIfNecessary(): void {
+        if (!this.activeTestStarted && this.testState == TestStateModel.CREATED)
+            this.$store.dispatch("startTest");
+    }
+}
 </script>
 
 <style lang="scss" scoped>
-    #text-input {
-        display: flex;
-        justify-content: center;
-        margin-top: 1em;
-        padding: 0.08em;
-        background-color: #a0a0a0;
-    }
+#text-input {
+    display: flex;
+    justify-content: center;
+    margin-top: 1em;
+    padding: 0.08em;
+    background-color: #a0a0a0;
+}
 
-    input {
-        width: 60%;
-    }
+input {
+    width: 60%;
+}
 </style>
